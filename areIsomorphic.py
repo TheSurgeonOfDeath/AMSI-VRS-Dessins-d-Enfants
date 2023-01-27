e7b5a11a9fd7c6950da6df2c8a76725409fb6c38
TheSurgeonOfDeath <83719688+TheSurgeonOfDeath@users.noreply.github.com>
2023-01-28 05:44:09 +1030

    Saving big dessin list objects as JSON

--- areIsomorphic.py
+++ areIsomorphic.py
@@ -1,4 +1,5 @@
-from Dessin import Dessin, findMorphisms
+from Dessin import Dessin, isMorphism
+from itertools import permutations
 
 fb = [(1,2,3,4)]
 fw = [(4,3,2,1)]
@@ -8,7 +9,11 @@ gb = [(2,1,3,4)]
 gw = [(4,3,1,2)]
 G = Dessin(gb, gw)
 
-
+def areIsomorphic(F, G):
+    if F.nEdges != G.nEdges:
+        return False
+    alphas = list(permutations(F.Edges))
+    return any(isMorphism(alpha, F, G) for alpha in alphas)
 
 print(areIsomorphic(F, G))
 
---
+++ bigDessins.py
@@ -0,0 +1,26 @@
+from Dessin import Dessin, generate_dessins
+import json, jsonpickle
+
+#### testing writing and reading json files of data
+# print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=2))
+# score = {'4': 5, '6': 7}
+# with open("data/testfile.json", 'w') as f:
+#     # indent=2 is not needed but makes the file human-readable 
+#     # if the data is nested
+#     json.dump(score, f, indent=2)
+
+# with open("data/testfile.json", 'r') as f:
+#     score2 = json.load(f)
+# print(score2)
+
+n = 5
+dessins = generate_dessins(n)
+for des in dessins: 
+    des.calcEulerChi()
+
+encoded_dessins = [jsonpickle.encode(des) for des in dessins]
+with open(f"data/dessins_order({n}).json", 'w') as f:
+    json.dump(encoded_dessins, f, indent=4)
+
+
+# print(len(dessins))
