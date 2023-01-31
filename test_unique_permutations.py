e3b3f2baefe2ee2e149cc11e5697e7bfe1c31389
TheSurgeonOfDeath <83719688+TheSurgeonOfDeath@users.noreply.github.com>
2023-01-31 20:34:36 +1030

    uncommented print statements

--- test_unique_permutations.py
+++ test_unique_permutations.py
@@ -2,5 +2,5 @@ from unique_permutations import unique_permutations
 p1 = list(unique_permutations((1,1,2,2)))
 p2 = [tuple(perm) for perm in unique_permutations((1,1,2,2))]
 
-# print(p1)
-# print(p2)
+print(p1)
+print(p2)
