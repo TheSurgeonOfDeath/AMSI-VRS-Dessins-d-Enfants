2d651f3e766851fd1b4d6a03a6b3dc66adf320ab
TheSurgeonOfDeath <83719688+TheSurgeonOfDeath@users.noreply.github.com>
2023-02-04 23:34:53 +1030

    trying to find nonisomorphic permutation pairs

--- nonisomomorphic_permutation_pairs.py
+++ nonisomomorphic_permutation_pairs.py
@@ -1,9 +1,10 @@
 from Dessin import Dessin, applyPermutation, array2cyclic
-from itertools import permutations, combinations_with_replacement
+from itertools import permutations, combinations_with_replacement, groupby
 from readableNestedList import readableNestedList
 import time
+import jsonpickle
 
-n = 3
+n = 4
 noniso_perm_pairs = []
 
 b = [tuple(range(1,n+1))]
@@ -14,6 +15,23 @@ w = b
 #     for 
 
 perms = [array2cyclic(perm) for perm in permutations(range(1, n+1))]
-permsSquared = list(combinations_with_replacement(perms, 2))
+# permsSquared = list(combinations_with_replacement(perms, 2))
 print(readableNestedList(perms))
-print(readableNestedList(permsSquared))
+# print(readableNestedList(permsSquared))
+
+# with open(f"data/dessins_order({n}).json", 'r') as f:
+#     dessins = jsonpickle.decode(f.read())
+
+# unique_desssins_mono = [des.mono for des in dessins]
+# unique_desssins_mono.pop(1)
+# unique_desssins_mono.pop(3)
+# print(readableNestedList(unique_desssins_mono))
+
+
+# unique_dessins_mono_indices = [permsSquared.index(tuple(mono)) for mono in unique_desssins_mono]
+# print(readableNestedList(unique_dessins_mono_indices))
+
+nonIsoPermPairs = []
+i_indices = range(len(perms))
+for i in i_indices:
+    nonIsoPermPairs.append((perms[i], perms[i]))
